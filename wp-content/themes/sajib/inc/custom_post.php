4e17a9ae49f57876cfbf1b1214ca2aa74d64290d
sajib <mhsajib44@gmail.com>
2024-10-06 10:44:58 +0600

    bxslider\

--- wp-content/themes/sajib/inc/custom_post.php
+++ wp-content/themes/sajib/inc/custom_post.php
@@ -1,5 +1,36 @@
 <?php
 
+function custom_slider(){
+  register_post_type ('slider',
+    array(
+      'labels' => array(
+        'name' => ('slider'),
+        'singular_name' => ('slider'),
+        'add_new' => ('Add New slider'),
+        'add_new_item' => ('Add New slider'),
+        'edit_item' => ('Edit slider'),
+        'new_item' => ('New slider'),
+        'view_item' => ('View slider'),
+        'not_found' => ('Sorry, we cound\'n find the slider you are looking for.'),
+      ),
+      'menu_icon' => 'dashicons-networking',
+      'public' => true,
+      'publicly_queryable' => true,
+      'exclude_from_search' => true,
+      'menu_position' => 5, 
+      'has_archive' => true,
+      'hierarchial' => true,
+      'show_ui' => true,
+      'capability_type' => 'post',
+      'rewrite' => array('slag' => 'slider'),
+      'supports' => array('title', 'thumbnail', 'editor', 'excerpt'),
+      )
+    );
+    add_theme_support('post-thumbnails');
+}
+add_action('init', 'custom_slider');
+
+
 function custom_service(){
   register_post_type ('service',
     array(
